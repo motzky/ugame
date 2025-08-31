64b4deae257bb8aeb2e0a0111627f71216c48c2a
Christian Moyzyczyk <motzky@gmx.net>
2025-08-31 19:56:00 +0200

    fix logging of cutstom type compile errors

--- include/log.h
+++ include/log.h
@@ -3,28 +3,31 @@
 #include <format>
 #include <source_location>
 #include <print>
-#include <string_view>
 
 namespace game::log
 {
-
     enum class Level
     {
         DEBUG,
         INFO,
         WARN,
+#ifndef WIN32
         ERROR
+#else
+        // We cannot use ERROR, if we included <windows.h> since that has a define for ERROR which then leads to compile errors here
+        ERR
+#endif
     };
 
     template <Level L, class... Args>
-    struct print
+    struct Print
     {
     };
 
     template <Level L, class... Args>
-    struct print<L, const char *, Args...>
+    struct Print<L, const char *, Args...>
     {
-        print(const char *msg, Args &&...args, std::source_location loc = std::source_location::current())
+        Print(const char *msg, Args &&...args, std::source_location loc = std::source_location::current())
         {
             auto level = "?";
             if constexpr (L == Level::DEBUG)
@@ -39,7 +42,11 @@ namespace game::log
             {
                 level = "WARN ";
             }
+#ifndef WIN32
             else if constexpr (L == Level::ERROR)
+#else
+            else if constexpr (L == Level::ERR)
+#endif
             {
                 level = "ERROR";
             }
@@ -49,17 +56,21 @@ namespace game::log
     };
 
     template <Level L, class... Args>
-    print(Args...) -> print<L, Args...>;
+    Print(Args &&...) -> Print<L, Args...>;
 
     template <class... Args>
-    using debug = print<Level::DEBUG, Args...>;
+    using debug = Print<Level::DEBUG, const char *, Args...>;
 
     template <class... Args>
-    using info = print<Level::INFO, Args...>;
+    using info = Print<Level::INFO, const char *, Args...>;
 
     template <class... Args>
-    using warn = print<Level::WARN, Args...>;
+    using warn = Print<Level::WARN, const char *, Args...>;
 
     template <class... Args>
-    using error = print<Level::ERROR, Args...>;
+#ifndef WIN32
+    using error = Print<Level::ERROR, const char *, Args...>;
+#else
+    using error = Print<Level::ERR, const char *, Args...>;
+#endif
 }
