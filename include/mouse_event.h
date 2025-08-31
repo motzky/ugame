2667c8d62da6b855f2832ebd9d244b701aee5f8d
Christian Moyzyczyk <motzky@gmx.net>
2025-09-01 01:26:03 +0200

    Add MouseEvent class

---
+++ include/mouse_event.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace game
+{
+
+    class MouseEvent
+    {
+
+    public:
+        MouseEvent(float delta_x, float delta_y);
+
+        auto delta_x() const -> float;
+        auto delta_x() const -> float;
+
+    private:
+        float _delta_x;
+        float _delta_y;
+    };
+}
---
+++ src/mouse_event.cpp
@@ -0,0 +1,19 @@
+#include "mouse_event.h"
+
+namespace game
+{
+
+    MouseEvent::MouseEvent(float delta_x, float delta_y)
+        : _delta_x(delta_x), _delta_y(delta_y)
+    {
+    }
+
+    auto MouseEvent::delta_x() const -> float
+    {
+        return _delta_x;
+    }
+    auto MouseEvent::delta_x() const -> float
+    {
+        return _delta_y;
+    }
+}
