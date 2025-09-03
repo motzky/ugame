c68d8d53aeb617928895c72b66ca75856309ce35
Christian Moyzyczyk <motzky@gmx.net>
2025-09-03 19:35:40 +0200

    Add StringUnorderedMap

---
+++ include/string_unordered_map.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace game
+{
+    struct StringHash
+    {
+        using is_transparent = void;
+        std::size_t operator()(const char *str) const
+        {
+            return std::hash<std::string_view>{}(str);
+        }
+
+        std::size_t operator()(std::string_view str) const
+        {
+            return std::hash<std::string_view>{}(str);
+        }
+
+        std::size_t operator()(const std::string &str) const
+        {
+            return std::hash<std::string_view>{}(str);
+        }
+    };
+
+    template <class T>
+    using StringUnorderedMap = std::unordered_map<std::string, T, StringHash, std::equal_to<>>;
+}
