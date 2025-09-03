46053f3cf1986e2fd491654cc94d421b7b460604
Christian Moyzyczyk <motzky@gmx.net>
2025-09-03 21:54:26 +0200

    Add std::formatter<> to MouseButtonEvent

--- include/mouse_button_event.h
+++ include/mouse_button_event.h
@@ -27,16 +27,37 @@ namespace game
     };
 }
 
-// template <>
-// struct std::formatter<game::MouseButtonEvent>
-// {
-//     constexpr auto parse(std::format_parse_context &ctx)
-//     {
-//         return std::begin(ctx);
-//     }
+template <>
+struct std::formatter<game::MouseButtonState>
+{
+    constexpr auto parse(std::format_parse_context &ctx)
+    {
+        return std::begin(ctx);
+    }
 
-//     auto format(const game::MouseButtonEvent &obj, std::format_context &ctx) const
-//     {
-//         return std::format_to(ctx.out(), "MouseButtonEvent {} {}", obj.delta_x(), obj.delta_y());
-//     }
-// };
+    auto format(const game::MouseButtonState &obj, std::format_context &ctx) const
+    {
+        switch (obj)
+        {
+            using enum game::MouseButtonState;
+        case UP:
+            return std::format_to(ctx.out(), "UP");
+        case DOWN:
+            return std::format_to(ctx.out(), "DOWN");
+        }
+    }
+};
+
+template <>
+struct std::formatter<game::MouseButtonEvent>
+{
+    constexpr auto parse(std::format_parse_context &ctx)
+    {
+        return std::begin(ctx);
+    }
+
+    auto format(const game::MouseButtonEvent &obj, std::format_context &ctx) const
+    {
+        return std::format_to(ctx.out(), "MouseButtonEvent: {} @ {} {}", obj.state(), obj.x(), obj.y());
+    }
+};
