9dd41cf2047d8489c942a8e99e1508ddce98c266
Christian Moyzyczyk <motzky@gmx.net>
2025-09-08 21:12:45 +0200

    Add cube map loading from TLV

--- include/cube_map.h
+++ include/cube_map.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <span>
+#include <string_view>
 #include <vector>
 
 #include "auto_release.h"
@@ -10,10 +12,13 @@
 
 namespace game
 {
+    class TlvReader;
+
     class CubeMap
     {
     public:
         CubeMap(const std::vector<std::span<const std::byte>> &faces, std::uint32_t width, std::uint32_t height);
+        CubeMap(const TlvReader &reader, std::array<std::string_view, 6> image_names);
 
         auto native_handle() const -> ::GLuint;
 
--- src/cube_map.cpp
+++ src/cube_map.cpp
@@ -1,5 +1,6 @@
 #include "cube_map.h"
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <ranges>
@@ -11,6 +12,28 @@
 #include "ensure.h"
 #include "log.h"
 #include "opengl.h"
+#include "tlv/tlv_reader.h"
+
+namespace
+{
+    auto num_channels_from_format(game::TextureFormat format) -> std::int32_t
+    {
+        switch (format)
+        {
+            using enum game::TextureFormat;
+        case R:
+            return 1;
+        case RGB:
+            return 3;
+        case RGBA:
+            return 4;
+        default:
+            game::ensure(false, "unhandled format {}", format);
+            return 0u;
+        }
+    }
+
+}
 
 namespace game
 {
@@ -63,6 +86,50 @@ namespace game
                 ensure(false, "unhandled num_channels {}", num_channels);
                 break;
             }
+        }
+    }
+
+    CubeMap::CubeMap(const TlvReader &reader, std::array<std::string_view, 6> image_names)
+        : _handle{0u, [](auto texture)
+                  { ::glDeleteTextures(1u, &texture); }}
+    {
+        const auto datas = image_names | std::views::transform(
+                                             [&reader](const auto &e)
+                                             {
+                                                 const auto data = std::ranges::find_if(reader, [e](const auto &f)
+                                                                                        { return f.is_texture(e); });
+                                                 ensure(data != std::ranges::end(reader), "failed to load texture");
+                                                 return (*data).texture_data_value();
+                                             });
+
+        const auto width = datas.front().width;
+        const auto height = datas.front().height;
+
+        ::glCreateTextures(GL_TEXTURE_CUBE_MAP, 1u, &_handle);
+        ::glTextureStorage2D(_handle, 1, GL_SRGB8, width, height);
+
+        for (const auto &[index, data] : std::views::enumerate(datas))
+        {
+            auto num_channels = num_channels_from_format(data.format);
+            auto format = GLenum{};
+
+            switch (num_channels)
+            {
+            case 1:
+                format = GL_RED;
+                break;
+            case 3:
+                format = GL_RGB;
+                break;
+            case 4:
+                format = GL_RGBA;
+                break;
+
+            default:
+                ensure(false, "unhandled num_channels {}", num_channels);
+                break;
+            }
+            ::glTextureSubImage3D(_handle, 0, 0, 0, static_cast<::GLuint>(index), data.width, data.height, 1, format, GL_UNSIGNED_BYTE, data.data.data());
         }
     }
 
