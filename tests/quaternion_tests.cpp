8654cdd31730aba8e456ab14a700247939c785ff
Christian Moyzyczyk <motzky@gmx.net>
2025-09-12 22:34:04 +0200

    Add quaternion multiplication test

--- tests/quaternion_tests.cpp
+++ tests/quaternion_tests.cpp
@@ -4,6 +4,7 @@
 #include <numbers>
 #include <print>
 
+#include "math/matrix4.h"
 #include "math/quaternion.h"
 
 TEST(quaternion, multiply)
@@ -20,6 +21,19 @@ TEST(quaternion, multiply)
     ASSERT_FLOAT_EQ(result.y, expected.y);
     ASSERT_FLOAT_EQ(result.z, expected.z);
     ASSERT_FLOAT_EQ(result.w, expected.w);
+}
+
+TEST(quaternion, multiply2)
+{
+    auto q1 = game::Quaternion{0.64207685f, 0.37862414f, 0.29617387f, 0.597212f};
+    auto q2 = game::Quaternion{0.7071f, 0.f, 0.f, 0.7071f};
+
+    auto result = q1 * q2;
+
+    ASSERT_FLOAT_EQ(result.x, 0.876301f);
+    ASSERT_FLOAT_EQ(result.y, 0.47714967f);
+    ASSERT_FLOAT_EQ(result.z, -0.05830057f);
+    ASSERT_FLOAT_EQ(result.w, -0.031723917f);
 }
 
 TEST(quaternion, multiply_inv)
