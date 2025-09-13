aab75d874f26794906000ec3535c477d4f4498f8
Christian Moyzyczyk <motzky@gmx.net>
2025-09-13 23:30:35 +0200

    Update Vector3 tests

--- tests/vector3_tests.cpp
+++ tests/vector3_tests.cpp
@@ -2,23 +2,36 @@
 
 #include "math/vector3.h"
 
+TEST(vector3, emtpy_ctor)
+{
+    const auto v = game::Vector3{};
+    ASSERT_FLOAT_EQ(v.x, 0.f);
+    ASSERT_FLOAT_EQ(v.y, 0.f);
+    ASSERT_FLOAT_EQ(v.z, 0.f);
+}
+
+TEST(vector3, single_value_ctor)
+{
+    const auto v = game::Vector3{1.1f};
+    ASSERT_FLOAT_EQ(v.x, 1.1f);
+    ASSERT_FLOAT_EQ(v.y, 1.1f);
+    ASSERT_FLOAT_EQ(v.z, 1.1f);
+}
+
+TEST(vector3, all_components_ctor)
+{
+    const auto v = game::Vector3{1.1f, 2.2f, 3.3f};
+    ASSERT_FLOAT_EQ(v.x, 1.1f);
+    ASSERT_FLOAT_EQ(v.y, 2.2f);
+    ASSERT_FLOAT_EQ(v.z, 3.3f);
+}
+
 TEST(vector3, negate)
 {
     const auto v1 = game::Vector3{1.f, 2.f, 3.f};
     const auto expected = game::Vector3{-1.f, -2.f, -3.f};
 
     ASSERT_EQ(-v1, expected);
-}
-
-TEST(vector3, add_zero)
-{
-    auto v1 = game::Vector3{1.f, 2.f, 3.f};
-    const auto zero = game::Vector3{0.f, 0.f, 0.f};
-    const auto expected = game::Vector3{1.f, 2.f, 3.f};
-
-    v1 += zero;
-
-    ASSERT_EQ(v1, expected);
 }
 
 TEST(vector3, normalize)
@@ -31,6 +44,28 @@ TEST(vector3, normalize)
     ASSERT_EQ(norm, expected);
 }
 
+TEST(vector3, normalize_point)
+{
+    auto v1 = game::Vector3{};
+    auto norm = game::Vector3::normalize(v1);
+
+    const auto expected = game::Vector3{};
+
+    ASSERT_EQ(norm, expected);
+}
+
+TEST(vector3, cross)
+{
+    const auto v1 = game::Vector3{1.f, 0.f, 0.f};
+    const auto v2 = game::Vector3{0.f, 1.f, 0.f};
+
+    const auto expected = game::Vector3{0.f, 0.f, 1.f};
+
+    const auto c = game::Vector3::cross(v1, v2);
+
+    ASSERT_EQ(c, expected);
+}
+
 TEST(vector3, multiply_assign_scalar)
 {
     auto v1 = game::Vector3{1.f, 2.f, 3.f};
@@ -40,4 +75,37 @@ TEST(vector3, multiply_assign_scalar)
     v1 *= 10.f;
 
     ASSERT_EQ(v1, expected);
+}
+
+TEST(vector3, add_zero)
+{
+    auto v1 = game::Vector3{1.f, 2.f, 3.f};
+    const auto zero = game::Vector3{};
+    const auto expected = game::Vector3{1.f, 2.f, 3.f};
+
+    v1 += zero;
+
+    ASSERT_EQ(v1, expected);
+}
+
+TEST(vector3, subtract_negated)
+{
+    const auto v1 = game::Vector3{1.f, 2.f, 3.f};
+    const auto v2 = v1 + (-v1);
+    const auto zero = game::Vector3{};
+
+    ASSERT_EQ(v2, zero);
+}
+
+TEST(vector3, subtract)
+{
+    const auto v1 = game::Vector3{10.f, 20.f, 30.f};
+    const auto v2 = game::Vector3{4.f, 5.f, 6.4f};
+    const auto expected = game::Vector3{6.f, 15.f, 23.6f};
+
+    auto v3 = v1 - v2;
+
+    ASSERT_EQ(v3.x, expected.x);
+    ASSERT_EQ(v3.y, expected.y);
+    ASSERT_EQ(v3.z, expected.z);
 }
