18fe4a8fa70f93325ebaa86d618695af0a489470
Christian Moyzyczyk <motzky@gmx.net>
2025-09-13 15:07:20 +0200

    Fix glext include on windows

--- include/graphics/opengl.h
+++ include/graphics/opengl.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #ifdef _WIN32
-#include "third_party/opengl/glext.h"
 #include <gl/gl.h>
+
+#include "../../../third_party/opengl/glext.h"
 #define GL_GET_PROC_ADDRESS wglGetProcAddress
 #else
 #include <GL/gl.h>
