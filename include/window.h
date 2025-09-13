bfc9daafbbbc33c338ec5d9e9fa28b384c796b03
Christian Moyzyczyk <motzky@gmx.net>
2025-09-13 20:58:27 +0200

    Fix indentation in window.h

--- include/window.h
+++ include/window.h
@@ -14,40 +14,40 @@
 
 namespace game
 {
-        class Window
-        {
-        public:
+    class Window
+    {
+    public:
 #ifdef _WIN32
-                typedef ::HWND HandleType;
+        typedef ::HWND HandleType;
 #else
-                typedef ::GLFWwindow *HandleType;
+        typedef ::GLFWwindow *HandleType;
 #endif
 
-                Window(std::uint32_t width, std::uint32_t height);
-                ~Window() = default;
+        Window(std::uint32_t width, std::uint32_t height);
+        ~Window() = default;
 
-                Window(const Window &) = delete;
-                Window &operator=(const Window &) = delete;
+        Window(const Window &) = delete;
+        Window &operator=(const Window &) = delete;
 
-                Window(Window &&) noexcept = default;
-                Window &operator=(Window &&) = default;
+        Window(Window &&) noexcept = default;
+        Window &operator=(Window &&) = default;
 
-                auto pump_event() -> std::optional<Event>;
-                auto swap() -> void;
+        auto pump_event() -> std::optional<Event>;
+        auto swap() -> void;
 
-                auto native_handle() const -> HandleType;
-                auto width() const -> std::uint32_t;
-                auto height() const -> std::uint32_t;
+        auto native_handle() const -> HandleType;
+        auto width() const -> std::uint32_t;
+        auto height() const -> std::uint32_t;
 
-                auto show_cursor(bool show) const -> void;
+        auto show_cursor(bool show) const -> void;
 
-        private:
-                AutoRelease<HandleType, nullptr> _windowHandle;
-                std::uint32_t _width;
-                std::uint32_t _height;
+    private:
+        AutoRelease<HandleType, nullptr> _windowHandle;
+        std::uint32_t _width;
+        std::uint32_t _height;
 #ifdef _WIN32
-                AutoRelease<::HDC> _dc;
-                ::WNDCLASSA _wc;
+        AutoRelease<::HDC> _dc;
+        ::WNDCLASSA _wc;
 #endif
-        };
+    };
 }
