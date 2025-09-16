1fba4a011a0cae8839f020dbf148e199f4351e87
Christian Moyzyczyk <motzky@gmx.net>
2025-09-16 23:12:55 +0200

    Improve Matrix3 tests

--- tests/matrix3_tests.cpp
+++ tests/matrix3_tests.cpp
@@ -32,6 +32,25 @@ TEST(matrix3, ctor_identity)
     ASSERT_FLOAT_EQ(m[8], vals[8]);
 }
 
+TEST(matrix3, ctor_from_three_vectors)
+{
+    const auto v1 = game::Vector3{3.f, 2.f, 1.f};
+    const auto v2 = game::Vector3{7.f, 8.f, 9.f};
+    const auto v3 = game::Vector3{4.f, 5.f, 6.f};
+
+    const auto m = game::Matrix3(v1, v2, v3);
+
+    ASSERT_FLOAT_EQ(m[0], 3.f);
+    ASSERT_FLOAT_EQ(m[1], 2.f);
+    ASSERT_FLOAT_EQ(m[2], 1.f);
+    ASSERT_FLOAT_EQ(m[3], 7.f);
+    ASSERT_FLOAT_EQ(m[4], 8.f);
+    ASSERT_FLOAT_EQ(m[5], 9.f);
+    ASSERT_FLOAT_EQ(m[6], 4.f);
+    ASSERT_FLOAT_EQ(m[7], 5.f);
+    ASSERT_FLOAT_EQ(m[8], 6.f);
+}
+
 TEST(matrix3, multiply_by_identity)
 {
     float vals[] = {
@@ -51,23 +70,15 @@ TEST(matrix3, multiply_by_identity)
     ASSERT_EQ(m1 * m2, m1);
 }
 
-TEST(matrix3, ctor_from_three_vectors)
+TEST(matrix3, multiply_by_matrix)
 {
-    const auto v1 = game::Vector3{3.f, 2.f, 1.f};
-    const auto v2 = game::Vector3{7.f, 8.f, 9.f};
-    const auto v3 = game::Vector3{4.f, 5.f, 6.f};
+    const auto m1 = game::Matrix3{{0.5f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f}};
+    const auto m2 = game::Matrix3{{0.5f, 1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f, 8.5f}};
 
-    const auto m = game::Matrix3(v1, v2, v3);
+    const auto expected = game::Matrix3{
+        {19.75f, 24.f, 28.5f, 48.25f, 60.f, 73.5f, 76.75f, 96.f, 118.5f}};
 
-    ASSERT_FLOAT_EQ(m[0], 3.f);
-    ASSERT_FLOAT_EQ(m[1], 2.f);
-    ASSERT_FLOAT_EQ(m[2], 1.f);
-    ASSERT_FLOAT_EQ(m[3], 7.f);
-    ASSERT_FLOAT_EQ(m[4], 8.f);
-    ASSERT_FLOAT_EQ(m[5], 9.f);
-    ASSERT_FLOAT_EQ(m[6], 4.f);
-    ASSERT_FLOAT_EQ(m[7], 5.f);
-    ASSERT_FLOAT_EQ(m[8], 6.f);
+    ASSERT_EQ(m1 * m2, expected);
 }
 
 TEST(matrix3, multiply_with_vector)
