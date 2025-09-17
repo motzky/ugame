5197913bce4cb55f73ab1dbecbdf831f73aec445
Christian Moyzyczyk <motzky@gmx.net>
2025-09-17 22:23:09 +0200

    Adapt  rendering of FrustumPlane lines

--- include/graphics/shape_wireframe_renderer.h
+++ include/graphics/shape_wireframe_renderer.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <array>
 #include <vector>
 
 #include "camera.h"
 #include "game/aabb.h"
+#include "game/frustum_plane.h"
+#include "graphics/color.h"
 #include "graphics/line_data.h"
 
 namespace game
@@ -11,6 +14,7 @@ namespace game
     class ShapeWireframeRenderer
     {
     public:
+        auto draw(const Vector3 &start, const Vector3 &end, const Color &color) -> void;
         auto draw(const Camera &camera) -> void;
         auto draw(const AABB &aabb) -> void;
         auto yield() -> std::vector<LineData>;
--- src/graphics/shape_wireframe_renderer.cpp
+++ src/graphics/shape_wireframe_renderer.cpp
@@ -1,28 +1,47 @@
 #include "graphics/shape_wireframe_renderer.h"
 
+#include <print>
 #include <ranges>
 #include <vector>
 
 #include "camera.h"
 #include "game/aabb.h"
+#include "game/frustum_plane.h"
 #include "graphics/color.h"
 #include "graphics/line_data.h"
+#include "math/matrix3.h"
+#include "math/vector3.h"
 
 namespace game
 {
-    auto ShapeWireframeRenderer::draw(const Camera & /*camera*/) -> void
+    auto ShapeWireframeRenderer::draw(const Vector3 &start, const Vector3 &end, const Color &color) -> void
     {
-        // auto col = Color{1.f, 0.f, 1.f};
-        // auto planes = camera.calculate_frustum_planes();
+        _lines.push_back({start, color});
+        _lines.push_back({end, color});
+    }
 
-        // _lines.push_back({camera.position(), col});
-        // _lines.push_back({planes[0].normal, col});
+    auto ShapeWireframeRenderer::draw(const Camera &camera) -> void
+    {
+        const auto corners = camera.frustum_corners();
 
-        // _lines.push_back({camera.position(), col});
+        const auto near_colour = Color{1.0f, 0.0f, 1.0f};
+        const auto far_colour = Color{1.0f, 0.0f, 0.0f};
+        const auto connect_colour = Color{1.0f, 1.0f, 0.0f};
 
-        // _lines.push_back({camera.position(), col});
+        draw(corners[0], corners[1], near_colour);
+        draw(corners[1], corners[2], near_colour);
+        draw(corners[2], corners[3], near_colour);
+        draw(corners[3], corners[0], near_colour);
 
-        // _lines.push_back({camera.position(), col});
+        draw(corners[4], corners[5], far_colour);
+        draw(corners[5], corners[6], far_colour);
+        draw(corners[6], corners[7], far_colour);
+        draw(corners[7], corners[4], far_colour);
+
+        draw(corners[0], corners[4], connect_colour);
+        draw(corners[1], corners[5], connect_colour);
+        draw(corners[2], corners[6], connect_colour);
+        draw(corners[3], corners[7], connect_colour);
     }
 
     auto ShapeWireframeRenderer::draw(const AABB &aabb) -> void
