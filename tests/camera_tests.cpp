1dfb6105d431187b62224a55d010de3c551bfc97
Christian Moyzyczyk <motzky@gmx.net>
2025-09-17 17:31:59 +0200

    Actually assert something in camera tests

--- tests/camera_tests.cpp
+++ tests/camera_tests.cpp
@@ -7,7 +7,7 @@
 
 TEST(camera, simple)
 {
-    auto camera = game::Camera{{0.f, 10.f, 0.f},
+    auto camera = game::Camera{{0.f, 0.f, 0.f},
                                {0.f, 0.f, -1.f},
                                {0.f, 1.f, 0.f},
                                std::numbers::pi_v<float> / 4.f,
@@ -17,11 +17,35 @@ TEST(camera, simple)
                                100.f};
 
     const auto planes = camera.calculate_frustum_planes();
-    for (const auto &plane : planes)
-    {
-        std::println("{}", plane);
-    }
+    const auto &[near, far, right, left, top, bottom] = planes;
 
-    // TODO Make real test
-    ASSERT_TRUE(true);
+    EXPECT_NEAR(near.normal.x, 0.f, 0.00001f);
+    EXPECT_NEAR(near.normal.y, 0.f, 0.00001f);
+    EXPECT_NEAR(near.normal.z, -1.f, 0.00001f);
+    EXPECT_NEAR(near.distance, .1f, 0.00001f);
+
+    EXPECT_NEAR(far.normal.x, 0.f, 0.00001f);
+    EXPECT_NEAR(far.normal.y, 0.f, 0.00001f);
+    EXPECT_NEAR(far.normal.z, 1.f, 0.00001f);
+    EXPECT_NEAR(far.distance, -100.f, 0.00001f);
+
+    EXPECT_NEAR(right.normal.x, 0.805234f, 0.00001f);
+    EXPECT_NEAR(right.normal.y, 0.f, 0.00001f);
+    EXPECT_NEAR(right.normal.z, -0.592958f, 0.00001f);
+    EXPECT_NEAR(right.distance, 0.f, 0.00001f);
+
+    EXPECT_NEAR(left.normal.x, -.805234f, 0.00001f);
+    EXPECT_NEAR(left.normal.y, 0.f, 0.00001f);
+    EXPECT_NEAR(left.normal.z, -.592958f, 0.00001f);
+    EXPECT_NEAR(left.distance, 0.f, 0.00001f);
+
+    EXPECT_NEAR(top.normal.x, 0.f, 0.00001f);
+    EXPECT_NEAR(top.normal.y, 0.923880f, 0.00001f);
+    EXPECT_NEAR(top.normal.z, -.382683f, 0.00001f);
+    EXPECT_NEAR(top.distance, 0.f, 0.00001f);
+
+    EXPECT_NEAR(bottom.normal.x, 0.f, 0.00001f);
+    EXPECT_NEAR(bottom.normal.y, -.923880f, 0.00001f);
+    EXPECT_NEAR(bottom.normal.z, -.382683f, 0.00001f);
+    EXPECT_NEAR(bottom.distance, 0.f, 0.00001f);
 }
