6b23a9f2e5011b01a9fdbc772c45ee8c9250be09
Christian Moyzyczyk <motzky@gmx.net>
2025-09-17 18:10:49 +0200

    Add formatter to color

--- include/graphics/color.h
+++ include/graphics/color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <format>
+
 namespace game
 {
     struct Color
@@ -10,3 +12,20 @@ namespace game
     };
 
 }
+
+template <>
+struct std::formatter<game::Color>
+{
+    constexpr auto parse(std::format_parse_context &ctx)
+    {
+        return std::begin(ctx);
+    }
+
+    auto format(const game::Color &obj, std::format_context &ctx) const
+    {
+        return std::format_to(ctx.out(), "r={} g={} b={}",
+                              obj.r,
+                              obj.g,
+                              obj.b);
+    }
+};
