1ec6a8f4fbc204ddefd7d902fa800ac925f4abf5
Christian Moyzyczyk <motzky@gmx.net>
2025-09-19 00:28:57 +0200

    Add expect() to ensure.h

--- include/ensure.h
+++ include/ensure.h
@@ -10,6 +10,25 @@
 
 namespace game
 {
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma waring(disable : 4702)
+#endif
+    template <class... Args>
+    auto expect(bool predicate, std::format_string<Args...> msg, Args &&...args) -> void
+    {
+        if (!predicate)
+        {
+            log::error("{}", std::format(msg, std::forward<Args>(args)...));
+            log::error("{}", std::stacktrace::current(2));
+            std::terminate();
+            std::unreachable();
+        }
+    }
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
     template <class... Args>
     auto ensure(bool predicate, std::string_view msg, Args &&...args) -> void
     {
