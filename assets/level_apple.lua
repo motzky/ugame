6fd9c7856181b22658e0bb890cd6f7cbf63509d2
Christian Moyzyczyk <motzky@gmx.net>
2025-09-27 21:24:38 +0200

    Implemented barrel rendering through lua definition

--- assets/level_apple.lua
+++ assets/level_apple.lua
@@ -1,5 +1,10 @@
+local barrels = {}
+
 function init_level()
     print('init_level');
+
+    barrels[1] = Vector3(0.0, -0.2, 0.0)
+    barrels[2] = Vector3(5.0, -0.2, 0.0)
 end
 
 function update_level(pos)
@@ -8,4 +13,12 @@ end
 
 function restart_level()
     print('restart_level');
+end
+
+function barrel_count()
+    return #barrels
+end
+
+function barrel_position(index)
+    return barrels[index]
 end
--- src/scripting/lua_level.cpp
+++ src/scripting/lua_level.cpp
@@ -17,8 +17,8 @@ namespace game::levels
 {
     LuaLevel::LuaLevel(const game::ResourceLoader &loader, std::string_view script_name, DefaultCache &resource_cache, const TlvReader &reader)
         : _script(loader.load(script_name).as_string()),
-        _entities{},
-    _floor{
+          _entities{},
+          _floor{
               resource_cache.get<Mesh>("floor"),
               resource_cache.get<Material>("floor_material"),
               {0.f, -2.f, 0},
@@ -32,6 +32,27 @@ namespace game::levels
     {
         const auto runner = ScriptRunner{_script};
         runner.execute("init_level");
+
+        const Texture *barrel_textures[]{
+            resource_cache.get<Texture>("barrel_albedo"),
+            resource_cache.get<Texture>("barrel_specular"),
+            resource_cache.get<Texture>("barrel_normal"),
+        };
+
+        const auto barrel_count = runner.execute<std::int64_t>("barrel_count");
+        for (std::int64_t i = 0; i < barrel_count; ++i)
+        {
+            const auto pos = runner.execute<Vector3>("barrel_position", i + 1);
+            _entities.emplace_back(game::Entity{_resource_cache.get<Mesh>("barrel"),
+                                                resource_cache.get<Material>("barrel_material"),
+                                                pos,
+                                                {0.4f},
+                                                {{0.f}, {1.f}, {0.707107f, 0.f, 0.f, 0.707107f}},
+                                                barrel_textures},
+                                   game::AABB{{pos.x - .6f, pos.y - .75f, pos.z - .6f}, {pos.x + .6f, pos.y + .75f, pos.z + .6f}},
+                                   //    std::make_unique<game::Chain<GameTransformState>>()
+                                   nullptr);
+        }
 
         _scene = game::Scene{
             .entities = _entities |
@@ -69,6 +90,11 @@ namespace game::levels
     {
         const auto runner = ScriptRunner{_script};
         runner.execute("restart_level");
+    }
+
+    auto LuaLevel::entities() const -> const std::vector<TransformedEntity> &
+    {
+        return _entities;
     }
 
 }
