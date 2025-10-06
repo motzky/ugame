8e6da5efae4fde81e3dcad7e07831a6d381ef5f4
Christian Moyzyczyk <motzky@gmx.net>
2025-10-06 20:58:27 +0200

    Implement workaround for github's gcc version (<15)

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,17 @@ auto main(int argc, char **argv) -> int
     game::log::info("starting game {}.{}.{}", game::version::major, game::version::minor, game::version::patch);
 
     const auto args = std::vector<std::string_view>(argv + 1u, argv + argc);
+
+#if defined(_MSC_VER) || defined(__GNUC__) && (__GNUC__ >= 15)
+    // only msvc and GCC >=15 support automatic formatting of std::vector
     game::log::info("args: {}", args);
+#else
+    for (const auto &s : args)
+    {
+        std::print("[{}} ", s);
+    }
+    std::printlin();
+#endif
 
     {
         auto g = game::Game{args};
