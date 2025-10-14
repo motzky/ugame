6f8c8bbf57429bd1f922beb70d0bcf16c2a8d626
Christian Moyzyczyk <motzky@gmx.net>
2025-10-14 20:53:50 +0200

    Fix text texture creation

--- src/graphics/text_factory.cpp
+++ src/graphics/text_factory.cpp
@@ -37,38 +37,64 @@ namespace game
 
         auto face = std::unique_ptr<FT_FaceRec_, decltype(releaseFace)>{};
 
-        const auto font_data = _resource_loader.load("CaskaydiaCoveNerdFont-Regular.ttf").as_bytes();
+        // const auto font_data = _resource_loader.load("CaskaydiaCoveNerdFont-Regular.ttf").as_bytes();
 
-        // ensure(::FT_New_Face(_ft, "CaskaydiaCoveNerdFont-Regular.ttf", 0, std::out_ptr(face)) == 0, "failed to load font");
-        ensure(::FT_New_Memory_Face(_ft, reinterpret_cast<const unsigned char *>(font_data.data()), font_data.size_bytes(), 0, std::out_ptr(face)) == 0, "failed to load font");
+        ensure(::FT_New_Face(_ft, "../assets/CaskaydiaCoveNerdFont-Regular.ttf", 0, std::out_ptr(face)) == 0, "failed to load font");
+        // ensure(::FT_New_Memory_Face(_ft, reinterpret_cast<const unsigned char *>(font_data.data()), font_data.size_bytes(), 0, std::out_ptr(face)) == 0, "failed to load font");
 
+        const auto pixel_size = 24u;
         // set pixel size to 0 x 48 -> 48x48
-        ::FT_Set_Pixel_Sizes(face.get(), 0, 48);
+        ensure(::FT_Set_Pixel_Sizes(face.get(), 0, pixel_size) == 0, "failed to set pixel sizes");
 
         auto tex_desc = TextureDescription{
             .name = std::string(text.data()),
-            .format = TextureFormat::RGB,
+            .format = TextureFormat::R,
             .usage = TextureUsage::SRGB,
-            .width{},
-            .height{},
+            .width{pixel_size * static_cast<std::uint32_t>(text.size())},
+            .height{pixel_size},
             .data{}};
+
+        tex_desc.data.resize(tex_desc.height * tex_desc.width, std::byte{});
+
+        auto old_width = 0u;
 
         for (auto i = 0u; i < text.size(); ++i)
         {
             const auto c = text.data()[i];
-            const auto index = ::FT_Get_Char_Index(face.get(), c);
-
-            ensure(::FT_Load_Char(face.get(), index, FT_LOAD_RENDER) == 0, "failed to load bitmap for character {}", c);
+            ensure(::FT_Load_Char(face.get(), c, FT_LOAD_RENDER) == 0, "failed to load bitmap for character {}", c);
 
             auto *glyph = face->glyph;
-            tex_desc.width += glyph->bitmap.width;
-            if (glyph->bitmap.rows > tex_desc.height)
+
+            log::debug("glyph {}: {}x{}", i, face->glyph->bitmap.width, face->glyph->bitmap.rows);
+
+            const auto bitmap_width = glyph->bitmap.width;
+            const auto width_padded = bitmap_width % 4 == 0 ? bitmap_width : bitmap_width + 4 - (bitmap_width % 4);
+
+            const auto y_offset = tex_desc.height - glyph->bitmap_top;
+
+            for (auto row = 0u; row < glyph->bitmap.rows; ++row)
             {
-                tex_desc.height = glyph->bitmap.rows;
+                for (std::uint32_t col = 0; col < width_padded; ++col)
+                {
+                    auto dst_x = old_width + col + glyph->bitmap_left;
+                    auto dst_y = y_offset == 0 ? row + 1 : y_offset + row - 1;
+                    auto dst_index = dst_y * tex_desc.width + dst_x;
+
+                    if (col >= bitmap_width)
+                    {
+                        tex_desc.data[dst_index] = std::byte{};
+                        continue;
+                    }
+
+                    auto src_index = row * glyph->bitmap.width + col;
+
+                    const auto val = glyph->bitmap.buffer[src_index];
+                    tex_desc.data[dst_index] = static_cast<std::byte>(val);
+                }
             }
 
-            // const auto buffer_size = glyph->bitmap.width * glyph->bitmap.rows;
-            // const auto stride = glyph->bitmap.pitch;
+            const auto advance_x = glyph->bitmap.width == 0 ? pixel_size / 2 : glyph->bitmap.width + glyph->bitmap_left;
+            old_width += advance_x;
         }
 
         return {tex_desc, sampler};
