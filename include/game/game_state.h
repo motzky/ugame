dc6f93a938d2297e7f82c60f3ac1b0420bbea86e
Christian Moyzyczyk <motzky@gmx.net>
2025-10-17 00:43:50 +0200

    Add new GameState and to_string() function

--- include/game/game_state.h
+++ include/game/game_state.h
@@ -1,10 +1,32 @@
 #pragma once
 
+#include <format>
+#include <string>
+#include <utility>
+
 namespace game
 {
     enum class GameState
     {
         RUNNING,
-        PAUSED
+        PAUSED,
+        EXITING
     };
+
+    inline auto to_string(GameState obj) -> std::string
+    {
+        switch (obj)
+        {
+            using enum GameState;
+        case RUNNING:
+            return "RUNNING";
+        case PAUSED:
+            return "PAUSED";
+        case EXITING:
+            return "EXITING";
+        default:
+            return std::format("{}", std::to_underlying(obj));
+        }
+    }
+
 }
