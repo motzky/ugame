da74143fb3c747162a2ae1b922f844b4cbc34103
Christian Moyzyczyk <motzky@gmx.net>
2025-10-16 20:35:03 +0200

    Rename base_routine to routine_base and use AutoSubscribe

--- include/game/routines/routine_base.h
+++ include/game/routines/routine_base.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <set>
+
 #include "game/game_state.h"
+#include "messaging/auto_subscribe.h"
 #include "messaging/message_bus.h"
 #include "messaging/subscriber.h"
 
@@ -9,7 +12,7 @@ namespace game
     class RoutineBase : public messaging::Subscriber
     {
     public:
-        RoutineBase(messaging::MessageBus &bus);
+        RoutineBase(messaging::MessageBus &bus, std::set<messaging::MessageType> types);
         virtual ~RoutineBase() = default;
 
         virtual auto handle_state_change(GameState state) -> void override;
@@ -17,5 +20,8 @@ namespace game
     protected:
         messaging::MessageBus &_bus;
         GameState _state;
+
+    private:
+        messaging::AutoSubscribe _auto_subscribe;
     };
 }
---
+++ src/game/routines/routine_base.cpp
@@ -0,0 +1,32 @@
+#include "game/routines/routine_base.h"
+
+#include <set>
+
+#include "game/game_state.h"
+#include "messaging/message_bus.h"
+#include "messaging/subscriber.h"
+
+namespace
+{
+    auto enrich_types(std::set<game::messaging::MessageType> types) -> std::set<game::messaging::MessageType>
+    {
+        types.emplace(game::messaging::MessageType::STATE_CHANGE);
+        return types;
+    }
+}
+
+namespace game
+{
+    RoutineBase::RoutineBase(messaging::MessageBus &bus, std::set<messaging::MessageType> types)
+        : _bus{bus},
+          _state{GameState::RUNNING},
+          _auto_subscribe{_bus, enrich_types(std::move(types)), this}
+    {
+    }
+
+    auto RoutineBase::handle_state_change(GameState state) -> void
+    {
+        _state = state;
+    }
+
+}
