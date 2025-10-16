75b223410d5c83514107be1a050d6327fb604d4e
Christian Moyzyczyk <motzky@gmx.net>
2025-10-16 20:18:35 +0200

    Subscribe to STATE_CHANGE message in constructor

--- src/game/routines/base_routine.cpp
+++ src/game/routines/base_routine.cpp
@@ -1,10 +1,15 @@
 #include "game/routines/base_routine.h"
 
+#include "game/game_state.h"
+#include "messaging/message_bus.h"
+#include "messaging/subscriber.h"
+
 namespace game
 {
     RoutineBase::RoutineBase(messaging::MessageBus &bus)
-        : _bus{bus}, _state{}
+        : _bus{bus}, _state{GameState::RUNNING}
     {
+        _bus.subscribe(messaging::MessageType::STATE_CHANGE, this);
     }
 
     auto RoutineBase::handle_state_change(GameState state) -> void
