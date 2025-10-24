8c383d9d572b181db11d06cc04f3901d20d36a05
Christian Moyzyczyk <motzky@gmx.net>
2025-10-25 00:37:24 +0200

    Add more default colors

--- include/graphics/color.h
+++ include/graphics/color.h
@@ -11,12 +11,18 @@ namespace game
         float g;
         float b;
 
-        static constexpr auto white() -> Color;
         static constexpr auto black() -> Color;
+        static constexpr auto blue() -> Color;
+        static constexpr auto green() -> Color;
+        static constexpr auto red() -> Color;
+        static constexpr auto white() -> Color;
     };
 
-    inline constexpr auto Color::white() -> Color { return {.r = 1.f, .g = 1.f, .b = 1.f}; }
     inline constexpr auto Color::black() -> Color { return {.r = 0.f, .g = 0.f, .b = 0.f}; }
+    inline constexpr auto Color::blue() -> Color { return {.r = 0.f, .g = 0.f, .b = 1.f}; }
+    inline constexpr auto Color::green() -> Color { return {.r = 0.f, .g = 1.f, .b = 0.f}; }
+    inline constexpr auto Color::red() -> Color { return {.r = 1.f, .g = 0.f, .b = 0.f}; }
+    inline constexpr auto Color::white() -> Color { return {.r = 1.f, .g = 1.f, .b = 1.f}; }
 
     inline auto to_string(Color obj) -> std::string
     {
