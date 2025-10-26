efc5254ce1ca73902d1939ac6fd17a4ffd5ef3ca
Christian Moyzyczyk <motzky@gmx.net>
2025-10-27 00:20:21 +0100

    Add settings for anisotropic filtering

--- include/game/settings.h
+++ include/game/settings.h
@@ -17,14 +17,22 @@ namespace game
         static auto destroy() -> void;
 
         auto width() const -> std::uint32_t;
-        auto height() const -> std::uint32_t;
-        auto anti_aliasing() const -> bool;
-        auto msaa_samples() const -> std::uint8_t;
-
         auto set_width(std::uint32_t value) -> void;
+
+        auto height() const -> std::uint32_t;
         auto set_height(std::uint32_t value) -> void;
+
+        auto anti_aliasing() const -> bool;
         auto set_anti_aliasing(bool enable) -> void;
+
+        auto msaa_samples() const -> std::uint8_t;
         auto set_msaa_samples(std::uint8_t samples) -> void;
+
+        auto anisotropic_filtering() const -> bool;
+        auto set_anisotropic_filtering(bool enable) -> void;
+
+        auto anisotropic_filter_samples() const -> std::uint8_t;
+        auto set_anisotropic_filter_samples(std::uint8_t samples) -> void;
 
     private:
         Settings();
@@ -34,5 +42,7 @@ namespace game
         std::uint32_t _height;
         bool _anti_aliasing;
         std::uint8_t _samples;
+        bool _anisotropic_filtering;
+        std::uint8_t _anisotropic_filter_samples;
     };
 }
--- src/game/settings.cpp
+++ src/game/settings.cpp
@@ -11,7 +11,9 @@ namespace game
         : _width{1280},
           _height(720),
           _anti_aliasing(false),
-          _samples(1)
+          _samples(1),
+          _anisotropic_filtering(true),
+          _anisotropic_filter_samples(16)
     {
     }
 
@@ -34,29 +36,24 @@ namespace game
         return _width;
     }
 
-    auto Settings::height() const -> std::uint32_t
-    {
-        return _height;
-    }
-
-    auto Settings::anti_aliasing() const -> bool
-    {
-        return _anti_aliasing;
-    }
-
-    auto Settings::msaa_samples() const -> std::uint8_t
-    {
-        return _samples;
-    }
-
     auto Settings::set_width(std::uint32_t value) -> void
     {
         _width = value;
     }
 
+    auto Settings::height() const -> std::uint32_t
+    {
+        return _height;
+    }
+
     auto Settings::set_height(std::uint32_t value) -> void
     {
         _height = value;
+    }
+
+    auto Settings::anti_aliasing() const -> bool
+    {
+        return _anti_aliasing;
     }
 
     auto Settings::set_anti_aliasing(bool enable) -> void
@@ -68,11 +65,36 @@ namespace game
         }
     }
 
+    auto Settings::msaa_samples() const -> std::uint8_t
+    {
+        return _samples;
+    }
+
     auto Settings::set_msaa_samples(std::uint8_t samples) -> void
     {
         expect(samples >= 1, "Invalid number of samples: {}", samples);
 
         _samples = samples;
+    }
+
+    auto Settings::anisotropic_filtering() const -> bool
+    {
+        return _anisotropic_filtering;
+    }
+
+    auto Settings::set_anisotropic_filtering(bool enable) -> void
+    {
+        _anisotropic_filtering = enable;
+    }
+
+    auto Settings::anisotropic_filter_samples() const -> std::uint8_t
+    {
+        return _anisotropic_filter_samples;
+    }
+
+    auto Settings::set_anisotropic_filter_samples(std::uint8_t samples) -> void
+    {
+        _anisotropic_filter_samples = samples;
     }
 
 }
